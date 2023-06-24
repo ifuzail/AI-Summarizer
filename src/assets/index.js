90fb8a048d08939c85dddee04832732e86be7e6b
Fuzail Khan <ifuzail1234@gmail.com>
2023-06-24 08:47:43 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import logo2 from './logo2.svg'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    logo2,
+    tick
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import {logo2} from '../assets'
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col" >
+      <nav className='flex justify-between items-center w-full mt-5 mb-10'>
+         <img src={logo2} alt='sumz_logo' className='w-24 object-contain'/>
+         
+
+         <button type='button' onClick={()=> window.open('https://github.com/adrianhajdin')} className='black_btn'>
+           GitHub
+         </button>
+      </nav>
+      <h1 className='head_text'>Summarize Article with <br className='max-md:hidden'/> <span className='teal_gradient'>OpenAI GPT-4</span>  
+      </h1>
+      <h2 className='desc'>Effortlessly Condense Complex Texts into Bite-Sized Insights with AI Summarizer</h2>
+    </header>
+  )
+}
+
+export default Hero
